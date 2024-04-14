2196f4bb67e1076f653f49a008892e30f6be7a6c
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-04-13 18:04:33 -0600

    feat: add exception for invalid login attempts

---
+++ EmployeeManagementSystem/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeManagementSystem.Exceptions;
+
+internal class InvalidLoginException : Exception
+{
+    public InvalidLoginException()
+    {
+    }
+
+    public InvalidLoginException(string? message) : base(message)
+    {
+    }
+
+    public InvalidLoginException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
