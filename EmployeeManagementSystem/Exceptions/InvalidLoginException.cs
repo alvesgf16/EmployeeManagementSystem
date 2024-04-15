da4f5f2518f42175c3e8795c1a44b0e05cf101ee
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-04-15 10:50:28 -0600

    refactor: make small aesthetic changes
    Add underscore before private field in services

--- EmployeeManagementSystem/Exceptions/InvalidLoginException.cs
+++ EmployeeManagementSystem/Exceptions/InvalidLoginException.cs
@@ -2,15 +2,9 @@
 
 internal class InvalidLoginException : Exception
 {
-    public InvalidLoginException()
-    {
-    }
+    public InvalidLoginException() { }
 
-    public InvalidLoginException(string? message) : base(message)
-    {
-    }
+    public InvalidLoginException(string? message) : base(message) { }
 
-    public InvalidLoginException(string? message, Exception? innerException) : base(message, innerException)
-    {
-    }
+    public InvalidLoginException(string? message, Exception? innerException) : base(message, innerException) { }
 }
--- EmployeeManagementSystem/Services/AuthService.cs
+++ EmployeeManagementSystem/Services/AuthService.cs
@@ -6,29 +6,28 @@ namespace EmployeeManagementSystem.Services;
 
 class AuthService
 {
-    private readonly SQLiteConnection database;
+    private readonly SQLiteConnection _database;
 
     public AuthService()
     {
-
-        database = new SQLiteConnection(Constants.DatabasePath);
-        database.CreateTable<User>();
+        _database = new SQLiteConnection(Constants.DatabasePath);
+        _database.CreateTable<User>();
     }
 
-    public List<User> GetUsers() => database.Table<User>().ToList();
+    public List<User> GetUsers() => _database.Table<User>().ToList();
 
     public User AuthenticateUserLogin(string username, string password)
     {
         if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)) throw new InvalidLoginException("Invalid username or password");
 
-        var userToAuth = database.Find<User>(username);
+        var userToAuth = _database.Find<User>(username);
 
         return userToAuth is not null && userToAuth.Password == password
             ? userToAuth
             : throw new InvalidLoginException("Invalid username or password");
     }
 
-    public void AddUser(string username, string password) => database.Insert(new User() { Username = username, Password = password });
+    public void AddUser(string username, string password) => _database.Insert(new User() { Username = username, Password = password });
 
-    public void DeleteUser(string username) => database.Delete<User>(username);
+    public void DeleteUser(string username) => _database.Delete<User>(username);
 }
