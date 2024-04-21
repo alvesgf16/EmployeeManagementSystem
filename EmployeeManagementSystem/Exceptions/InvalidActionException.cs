1ce0f2238f7c1854fd150e5035dfbf00fa0e6ce0
Juan Sebastian Gualtero Villamil <JuanSebastian.GualteroVillamil@edu.sait.ca>
2024-04-20 19:24:29 -0600

    Added exception to handle invalid actions on ManagerEmployeeDetailsView

---
+++ EmployeeManagementSystem/Exceptions/InvalidActionException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Exceptions
+{
+    internal class InvalidActionException : Exception
+    {
+        public InvalidActionException() { }
+
+        public InvalidActionException(string? message) : base(message) { }
+
+        public InvalidActionException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
